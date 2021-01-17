8c2cb4a1613ebb09880cebab18a98774240d8438
John Moustakas <john.moustakas@gmail.com>
2021-01-17 07:37:26 -0800

    another typo

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 import os.path
-
+from importlib import import_module
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
