c5c797bdd50b0875e36105eea472ebbc5ef90033
John Moustakas <john.moustakas@gmail.com>
2023-02-17 12:53:37 -0800

    get ready for 2.1.0

--- bin/mpi-fastspecfit.sh
+++ bin/mpi-fastspecfit.sh
@@ -36,12 +36,14 @@ mpiscript=$codedir/fastspecfit/bin/mpi-fastspecfit
 
 echo 'Loading DESI software stack 23.1'
 source /global/cfs/cdirs/desi/software/desi_environment.sh 23.1
+module swap desispec/0.57.0
+module load fastspecfit/2.1.0
 
-for package in fastspecfit; do
-    echo Loading local check-out of $package
-    export PATH=$codedir/$package/bin:$PATH
-    export PYTHONPATH=$codedir/$package/py:$PYTHONPATH
-done
+#for package in desispec fastspecfit; do
+#    echo Loading local check-out of $package
+#    export PATH=$codedir/$package/bin:$PATH
+#    export PYTHONPATH=$codedir/$package/py:$PYTHONPATH
+#done
 
 outdir_data=/global/cfs/cdirs/desi/spectro/fastspecfit
 outdir_html=/global/cfs/cdirs/desi/users/ioannis/fastspecfit
