91f684e374d1ce268bf924a377a1aa2e374143fb
John Moustakas <john.moustakas@gmail.com>
2023-02-27 11:55:23 -0800

    begin migration to postgres db

--- py/fastspecfit/webapp/settings.py
+++ py/fastspecfit/webapp/settings.py
@@ -89,11 +89,21 @@ WSGI_APPLICATION = 'fastspecfit.webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'fastspecfit',
+       'USER': 'fastspecfit',
+       'PASSWORD': os.environ.get('POSTGRES_DB_PASSWORD'),
+       'HOST': 'db',
+   }
 }
 
 # Password validation
