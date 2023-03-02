67347002b8bf6d03eba40d2cb93184dd0c972f5b
John Moustakas <john.moustakas@gmail.com>
2023-03-01 16:23:08 -0800

    begin renaming app

--- py/fastspecfit/webapp/sample/app.py
+++ py/fastspecfit/webapp/sample/app.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 from fastspecfit.webapp.sample.templatetags import *
 
 class FastSpecFitApp(AppConfig):
-    name = 'sample'
+    name = 'fastmodel'
--- py/fastspecfit/webapp/urls.py
+++ py/fastspecfit/webapp/urls.py
@@ -20,20 +20,12 @@ from django.contrib import admin
 from django.urls import re_path, path
 from django.views.generic import TemplateView
 
-import fastspecfit.webapp.sample.views as sample
+import fastspecfit.webapp.sample.views as fastmodel
 
 urlpatterns = [
-    re_path('^$', sample.explore, name='index'),
-    re_path(r'target/(?P<target_name>[^/]*)\Z', sample.target, name='target'),
-    re_path(r'target-prev/(\d+)$', sample.target_prev, name='target-prev'),
-    re_path(r'target-next/(\d+)$', sample.target_next, name='target-next'),
-    re_path(r'upload-catalog$', sample.upload_catalog, name='upload-catalog'),
+    re_path('^$', fastmodel.explore, name='index'),
+    re_path(r'target/(?P<target_name>[^/]*)\Z', fastmodel.target, name='target'),
+    re_path(r'target-prev/(\d+)$', fastmodel.target_prev, name='target-prev'),
+    re_path(r'target-next/(\d+)$', fastmodel.target_next, name='target-next'),
+    re_path(r'upload-catalog$', fastmodel.upload_catalog, name='upload-catalog'),
 ]
-    
-# urlpatterns = [
-#     re_path(r'^$', sample.explore, name='index'),
-#     re_path(r'^target/(.+)$', sample.target, name='target'),
-#     re_path(r'^target-prev/(\d+)$', sample.target_prev, name='target-prev'),
-#     re_path(r'^target-next/(\d+)$', sample.target_next, name='target-next'),
-# ]
-
