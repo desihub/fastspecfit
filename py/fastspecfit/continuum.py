648f93d3319d6ed4bf7a91da820e456f6d3d9dad
jdbuhler <buhlej@rpi.edu>
2024-07-21 19:03:54 -0400

    * revert _smooth_continuum impl per John

--- py/fastspecfit/continuum.py
+++ py/fastspecfit/continuum.py
@@ -67,8 +67,8 @@ def convolve_vdisp(templateflux, vdisp, pixsize_kms=PIXKMS_BLU, limit=None):
     return output
 
 
-def _smooth_continuum(wave, flux, ivar, linemask, camerapix=None, 
-                      png=None, log=None):
+def _smooth_continuum(wave, flux, ivar, linemask, camerapix=None, medbin=175, 
+                      smooth_window=75, smooth_step=125, png=None, log=None):
     """Build a smooth, nonparametric continuum spectrum.
 
     Parameters
@@ -79,13 +79,23 @@ def _smooth_continuum(wave, flux, ivar, linemask, camerapix=None,
         Spectrum corresponding to `wave`.
     ivar : :class:`numpy.ndarray` [npix]
         Inverse variance spectrum corresponding to `flux`.
-    linemask : :class:`numpy.ndarray` of type :class:`bool`
+    linemask : :class:`numpy.ndarray` of type :class:`bool`, optional, defaults to `None`
         Boolean mask with the same number of pixels as `wave` where `True`
-        means a pixel is affected by an emission line.
-    camerapix : :class:`list` [ncamera]
-        List of starting and ending :class:`int` pixel indices indicating
-         the range of each camera.
-    png : :class:`str`
+        means a pixel is (possibly) affected by an emission line
+        (specifically a strong line which likely cannot be median-smoothed).
+    medbin : :class:`int`, optional, defaults to 150 pixels
+        Width of the median-smoothing kernel in pixels; a magic number.
+    smooth_window : :class:`int`, optional, defaults to 50 pixels
+        Width of the sliding window used to compute the iteratively clipped
+        statistics (mean, median, sigma); a magic number. Note: the nominal
+        extraction width (0.8 A) and observed-frame wavelength range
+        (3600-9800 A) corresponds to pixels that are 66-24 km/s. So
+        `smooth_window` of 50 corresponds to 3300-1200 km/s, which is
+        conservative for all but the broadest lines. A magic number. 
+    smooth_step : :class:`int`, optional, defaults to 10 pixels
+        Width of the step size when computing smoothed statistics; a magic
+        number.
+    png : :class:`str`, optional, defaults to `None`
         Generate a simple QA plot and write it out to this filename.
 
     Returns
@@ -95,33 +105,111 @@ def _smooth_continuum(wave, flux, ivar, linemask, camerapix=None,
         order to create a pure emission-line spectrum.
 
     """
-    from scipy.interpolate import splev, splrep
+    from numpy.lib.stride_tricks import sliding_window_view
+    from scipy.ndimage import median_filter
+    from fastspecfit.util import sigmaclip
+    from scipy.interpolate import make_interp_spline
         
     if log is None:
         from desiutil.log import get_logger
         log = get_logger()
 
-    if len(linemask) != len(wave):
+    npix = len(wave)
+
+    if len(linemask) != npix:
         errmsg = 'Linemask must have the same number of pixels as the input spectrum.'
         log.critical(errmsg)
         raise ValueError(errmsg)
 
-    srt = np.argsort(wave)
-    spl = splrep(wave[srt], flux[srt], w=np.sqrt(ivar[srt]), k=3)
-    smooth = splev(wave, spl)
+    # If camerapix is given, mask the 10 (presumably noisy) pixels from the edge
+    # of each per-camera spectrum.
+    if camerapix is not None:
+        for campix in camerapix:
+            ivar[campix[0]:campix[0]+10] = 0.
+            ivar[campix[1]-10:campix[1]] = 0.
 
-    ## Treat each camera independently. Assume wavelengths are sorted!
-    #if camerapix is not None:
-    #    smooth = []
-    #    for pix in camerapix:
-    #        spl = splrep(wave[pix[0]:pix[1]], flux[pix[0]:pix[1]],
-    #                     w=np.sqrt(ivar[pix[0]:pix[1]]), k=3)
-    #        smooth.append(splev(wave[pix[0]:pix[1]], spl))
-    #    smooth = np.hstack(smooth)
-    #else:
-    #    spl = splrep(wave, flux, w=np.sqrt(ivar), k=3)
-    #    smooth = splev(wave, spl)
+    # Build the smooth (line-free) continuum by computing statistics in a
+    # sliding window, accounting for masked pixels and trying to be smart
+    # about broad lines. See:
+    #   https://stackoverflow.com/questions/41851044/python-median-filter-for-1d-numpy-array
+    #   https://numpy.org/devdocs/reference/generated/numpy.lib.stride_tricks.sliding_window_view.html
+    wave_win = sliding_window_view(wave, window_shape=smooth_window)
+    flux_win = sliding_window_view(flux, window_shape=smooth_window)
+    ivar_win = sliding_window_view(ivar, window_shape=smooth_window)
+    noline_win = sliding_window_view(np.logical_not(linemask), window_shape=smooth_window)
 
+    nminpix = 15
+
+    smooth_wave, smooth_flux, smooth_sigma = [], [], []
+    for swave, sflux, sivar, noline in zip(wave_win[::smooth_step],
+                                           flux_win[::smooth_step],
+                                           ivar_win[::smooth_step],
+                                           noline_win[::smooth_step]):
+
+        # if there are fewer than XX good pixels after accounting for the
+        # line-mask, skip this window.
+        sflux = sflux[noline]
+        if len(sflux) < nminpix:
+            continue
+
+        cflux, clo, chi = sigmaclip(sflux, low=2.0, high=2.0)
+        if len(cflux) < nminpix:
+            continue
+        
+        sivar = sivar[noline]
+        # Toss out regions with too little good data.
+        if np.sum(sivar > 0) < nminpix:
+            continue
+        
+        sig = np.std(cflux) # simple median and sigma
+        mn = median(cflux)
+
+        # One more check for crummy spectral regions.
+        if mn == 0.0:
+            continue
+
+        swave = swave[noline]
+        I = ((sflux >= clo) & (sflux <= chi))
+        smooth_wave.append(np.mean(swave[I]))
+        
+        smooth_sigma.append(sig)
+        smooth_flux.append(mn)
+
+    smooth_wave = np.array(smooth_wave)
+    smooth_sigma = np.array(smooth_sigma)
+    smooth_flux = np.array(smooth_flux)
+
+    # For debugging.
+    if png:
+        _smooth_wave = smooth_wave.copy()
+        _smooth_flux = smooth_flux.copy()
+
+    # corner case for very wacky spectra
+    if len(smooth_flux) == 0:
+        smooth_flux = flux
+        #smooth_sigma = flux * 0 + np.std(flux)
+    else:
+        #smooth_flux = np.interp(wave, smooth_wave, smooth_flux)
+        #smooth_sigma = np.interp(wave, smooth_wave, smooth_sigma)
+        _, uindx = np.unique(smooth_wave, return_index=True)
+        srt = np.argsort(smooth_wave[uindx])
+        bspl_flux = make_interp_spline(smooth_wave[uindx][srt], smooth_flux[uindx][srt], k=1)
+        smooth_flux = bspl_flux(wave)
+
+        # check for extrapolation
+        blu = np.where(wave < np.min(smooth_wave[srt]))[0]
+        red = np.where(wave > np.max(smooth_wave[srt]))[0]
+        if len(blu) > 0:
+            smooth_flux[:blu[-1]] = smooth_flux[blu[-1]]
+        if len(red) > 0:
+            smooth_flux[red[0]:] = smooth_flux[red[0]]
+
+    smooth = median_filter(smooth_flux, medbin, mode='nearest')
+    
+    # very important!
+    Z = (flux == 0.0) * (ivar == 0.0)
+    if np.sum(Z) > 0:
+        smooth[Z] = 0.0
 
     # Optional QA.
     if png:
@@ -138,9 +226,12 @@ def _smooth_continuum(wave, flux, ivar, linemask, camerapix=None,
         ax[0].scatter(wave[linemask] / 1e4, flux[linemask], s=10, marker='s', 
                       color='k', zorder=2, alpha=0.5, label='Line-masked pixel')
         ax[0].plot(wave / 1e4, smooth, color='red', label='Smooth continuum')
+        #ax[0].plot(_smooth_wave / 1e4, _smooth_flux, color='orange')
+        #ax[0].plot(wave, median_filter(flux, medbin, mode='nearest'), color='k', lw=2)
         ax[0].set_ylim(np.min((-5. * noise, quantile(flux, 0.05))), 
                        np.max((5. * noise, 1.5 * quantile(flux, 0.975))))
         ax[0].set_ylabel('Continuum-subtracted Spectrum')
+        #ax[0].scatter(_smooth_wave, _smooth_flux, color='orange', marker='s', ls='-', s=20)
         ax[0].legend(fontsize=12)
 
         ax[1].plot(wave / 1e4, resid, alpha=0.75)#, label='Residuals')
