aff781ebb92d7b3c886b455b8db5833e705bd05c
jdbuhler <buhlej@rpi.edu>
2024-07-28 02:38:32 -0400

    add missing linetable.py file from previous commit

---
+++ py/fastspecfit/linetable.py
@@ -0,0 +1,39 @@
+#
+# Emission line table, read from file
+#
+
+import os
+
+from astropy.table import Table
+
+from fastspecfit.logger import log
+
+class LineTable(object):
+
+    def __init__(self, emlines_file=None):
+        
+        """Read the set of emission lines of interest.
+        
+        """
+        if emlines_file is None:
+            # use default emlines location
+            from importlib import resources
+            emlines_file = resources.files('fastspecfit').joinpath('data/emlines.ecsv')
+        
+        if not os.path.isfile(emlines_file):
+            errmsg = f'Emission lines parameter file {emlines_file} does not exist.'
+            log.critical(errmsg)
+            raise ValueError(errmsg)
+
+        self.file = emlines_file
+        
+        try:
+            linetable = Table.read(emlines_file, format='ascii.ecsv', guess=False)
+        except: 
+            errmsg = f'Problem reading emission lines parameter file {emlines_file}.'
+            log.critical(errmsg)
+            raise ValueError(errmsg)
+
+        linetable.sort('restwave')
+        
+        self.table = linetable
