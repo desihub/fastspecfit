6d30d34bbdba734b08a11b3606458350e9a6f401
John Moustakas <john.moustakas@gmail.com>
2024-12-05 10:17:55 -0500

    blarg, did not actually fix the problem

--- py/fastspecfit/test/test_fastspecfit.py
+++ py/fastspecfit/test/test_fastspecfit.py
@@ -11,7 +11,7 @@ import pytest
 def filenames(outdir):
     from importlib import resources
 
-    reduxdir = resources.files('fastspecfit').joinpath('test/data')
+    redux_dir = resources.files('fastspecfit').joinpath('test/data')
     specproddir = resources.files('fastspecfit').joinpath('test/data')
     mapdir = resources.files('fastspecfit').joinpath('test/data')
     fphotodir = resources.files('fastspecfit').joinpath('test/data')
@@ -19,9 +19,9 @@ def filenames(outdir):
     fastspec_outfile = os.path.join(outdir, 'fastspec.fits')
     fastphot_outfile = os.path.join(outdir, 'fastphot.fits')
 
-    filenames = {'reduxdir': reduxdir, 'specproddir': specproddir, 'mapdir': mapdir,
-                 'fphotodir': fphotodir, 'redrockfile': redrockfile,
-                 'fastspec_outfile': fastspec_outfile,
+    filenames = {'redux_dir': redux_dir, 'specproddir': specproddir,
+                 'mapdir': mapdir, 'fphotodir': fphotodir,
+                 'redrockfile': redrockfile, 'fastspec_outfile': fastspec_outfile,
                  'fastphot_outfile': fastphot_outfile, }
 
     yield filenames
@@ -36,6 +36,7 @@ def test_fastphot(filenames, templates):
 
     cmd = f'fastphot {filenames["redrockfile"]} -o {outfile} ' + \
         f'--mapdir {filenames["mapdir"]} --fphotodir {filenames["fphotodir"]} ' + \
+        f'--redux_dir {filenames["redux_dir"]} ' + \
         f'--specproddir {filenames["specproddir"]} --templates {templates}'
 
     args = parse(options=cmd.split()[1:])
@@ -57,6 +58,7 @@ def test_fastspec(filenames, templates):
     outfile = filenames["fastspec_outfile"]
 
     cmd = f'fastspec {filenames["redrockfile"]} -o {outfile} ' + \
+        f'--redux_dir {filenames["redux_dir"]} ' + \
         f'--mapdir {filenames["mapdir"]} --fphotodir {filenames["fphotodir"]} ' + \
         f'--specproddir {filenames["specproddir"]} --templates {templates}'
 
