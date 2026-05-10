3f232e458ad1bf38e8ad7e3a509def59b65d14a5
John Moustakas <john.moustakas@gmail.com>
2026-05-10 07:04:02 -0700

    revert further back

--- py/fastspecfit/emline_fit/utils.py
+++ py/fastspecfit/emline_fit/utils.py
@@ -16,7 +16,9 @@ def norm_pdf(a):
     """
     PDF of standard normal distribution at a point a
     """
+
     SQRT_2PI = np.sqrt(2 * np.pi)
+
     return 1/SQRT_2PI * np.exp(-0.5 * a**2)
 
 
@@ -25,9 +27,20 @@ def norm_cdf(a):
     """
     Approximate the integral of a standard normal PDF from -infty to a.
     """
+
     SQRT1_2 = 1.0 / np.sqrt(2)
+
     z = np.abs(a)
 
+    # Optimization (currently disabled because it is not needed): If
+    # |a| > MAX_SDEV, treat the value as extreme and return 0 or 1 as
+    # appropriate.
+
+    #if z > MAX_SDEV: # short-circuit extreme values
+    #    if a > 0:
+    #        y = 1.
+    #    else:
+    #        y = 0.
     if z < 1.:
         y = 0.5 + 0.5 * erf(a * SQRT1_2)
     else:
@@ -42,10 +55,27 @@ def norm_cdf(a):
 def max_buffer_width(log_obs_bin_edges, line_sigmas, padding=0):
     """
     Compute a safe estimate of the number of nonzero bin fluxes possible
-    for a line spanning a subrange of bins with edges log_obs_bin_edges.
+    for a line spanning a subrange of bins with edges log_obs_bin_edges,
+    assuming the line's width is one of the values in line_sigmas.
+    Optionally add 2*padding to allow future expansion to left and right.
+
+    Parameters
+    ----------
+    log_obs_bin_edges : :class:`np.ndarray` [# obs wavelength bins + 1]
+      log of wavelengths of all observed bin edges.
+    line_sigmas : :class:`np.ndarray`  [# nlines]
+      Gaussian widths of all spectral lines.
+    padding : :class:`int`
+      Padding parameter to add to width for future use.
+
     """
+
+    # Find the largest width sigma for any line, and
+    # allocate enough space for twice that much width
+    # in bins, given the smallest observed bin width.
+    # Add padding and a little fudge factor to be safe.
     max_width = \
-        int(2 * MAX_SDEV * np.max(line_sigmas / C_LIGHT) /
+        int(2*MAX_SDEV*np.max(line_sigmas/C_LIGHT) / \
             np.min(np.diff(log_obs_bin_edges))) + \
-            2 * padding + 4
+            2*padding + 4
     return max_width
